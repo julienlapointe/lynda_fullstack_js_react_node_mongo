e36e70e3f143bbbb3b8e5a167559c6f73940b381
Julien LaPointe <email@julienlapointe.com>
2017-08-12 22:14:34 -0500

    introduced state into the React Component class and played with the Component lifecycle methods componentDidMount() and componentWillUnmount()

--- src/component.js
+++ src/component.js
@@ -1,121 +1,133 @@
 // index.js should do 1 thing: render the top-level component (App) to the DOM
 
-import React from "react";
-import ReactDOM from "react-dom";
+// moved imports to bottom where the Component is...
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-// .render() has 2 parameters:
-	// param #1: what to render
-	// param #2: where to render it
-// .createElement() has 3 parameters:
-	// param #1: HTML element tag
-	// param #2: HTML element attribute(s)
-	// param #3+: child elements of this element
-ReactDOM.render(
-	React.createElement("h2", null, "Hello from React!"),
-	document.getElementById("root1")
-);
+// // .render() has 2 parameters:
+// 	// param #1: what to render
+// 	// param #2: where to render it
+// // .createElement() has 3 parameters:
+// 	// param #1: HTML element tag
+// 	// param #2: HTML element attribute(s)
+// 	// param #3+: child elements of this element
+// ReactDOM.render(
+// 	React.createElement("h2", null, "Hello from React!"),
+// 	document.getElementById("root1")
+// );
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-ReactDOM.render(
-	<h2>Hello from React with JSX! {Math.random()}</h2>,
-	document.getElementById("root2")
-);
+// ReactDOM.render(
+// 	<h2>Hello from React with JSX! {Math.random()}</h2>,
+// 	document.getElementById("root2")
+// );
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-const color = Math.random() > 0.5 ? "green" : "red";
+// const color = Math.random() > 0.5 ? "green" : "red";
 
-ReactDOM.render(
-	<h2 style={{color: color}}>
-		Hello from React with JSX w/ Style Variables!
-	</h2>,
-	document.getElementById("root3")
-);
+// ReactDOM.render(
+// 	<h2 style={{color: color}}>
+// 		Hello from React with JSX w/ Style Variables!
+// 	</h2>,
+// 	document.getElementById("root3")
+// );
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-// this component accepts props to be outputted in JSX
-const App1 = (props) => {
-	return(
-		<div>
-			<h2 className="text-center">
-				Hello from React with JSX via a Component! {props.message}
-			</h2>
-			<div>
-				...
-			</div>
-		</div>
-	);
-};
+// // this component accepts props to be outputted in JSX
+// const App1 = (props) => {
+// 	return(
+// 		<div>
+// 			<h2 className="text-center">
+// 				Hello from React with JSX via a Component! {props.message}
+// 			</h2>
+// 			<div>
+// 				...
+// 			</div>
+// 		</div>
+// 	);
+// };
 
-ReactDOM.render(
-	<App1 />,
-	document.getElementById("root4")
-);
+// ReactDOM.render(
+// 	<App1 />,
+// 	document.getElementById("root4")
+// );
 
-// "prop validation"
-// message: React.PropTypes.string = props.message must be a string (throw an error if a number or other "primitive" is passed in)
-// .isRequired = throw an error if the value for props.message is missing / empty
-App1.propTypes = {
-	message: React.PropTypes.string.isRequired
-};
+// // "prop validation"
+// // message: React.PropTypes.string = props.message must be a string (throw an error if a number or other "primitive" is passed in)
+// // .isRequired = throw an error if the value for props.message is missing / empty
+// App1.propTypes = {
+// 	message: React.PropTypes.string.isRequired
+// };
 
-// set a default value for props.message
-App1.defaultProps = {
-	message: "I'm the default value for the props.message!"
-};
+// // set a default value for props.message
+// App1.defaultProps = {
+// 	message: "I'm the default value for the props.message!"
+// };
 
-ReactDOM.render(
-	<App1 message="And from props!" />,
-	document.getElementById("root5")
-);
+// ReactDOM.render(
+// 	<App1 message="And from props!" />,
+// 	document.getElementById("root5")
+// );
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-// const Header2 = (props) => {
+// // const Header2 = (props) => {
+// // 	return(
+// // 		<h2 className="text-center">
+// // 			{props.message2}
+// // 		</h2>
+// // 	);
+// // };
+
+// // used destructuring to access props.message2
+// const Header2 = ({message2}) => {
 // 	return(
 // 		<h2 className="text-center">
-// 			{props.message2}
+// 			{message2}
 // 		</h2>
 // 	);
 // };
 
-// used destructuring to access props.message2
-const Header2 = ({message2}) => {
-	return(
-		<h2 className="text-center">
-			{message2}
-		</h2>
-	);
-};
+// // validation for a prop (ex. props.message2 validation below) must take place *after* the Component definition that uses that prop (ex. Header2 defined above)
+// Header2.propTypes = {
+// 	message2: React.PropTypes.string.isRequired
+// };
 
-// validation for a prop (ex. props.message2 validation below) must take place *after* the Component definition that uses that prop (ex. Header2 defined above)
-Header2.propTypes = {
-	message2: React.PropTypes.string.isRequired
-};
+// const App2 = () => {
+// 	return(
+// 		<div className="App2">
+// 			<Header2 message2="Hello from a Component within a Component!" />
+// 			<div>
+// 				...
+// 			</div>
+// 		</div>
+// 	);
+// };
 
-const App2 = () => {
-	return(
-		<div className="App2">
-			<Header2 message2="Hello from a Component within a Component!" />
-			<div>
-				...
-			</div>
-		</div>
-	);
-};
-
-ReactDOM.render(
-	<App2 />,
-	document.getElementById("root6")
-);
+// ReactDOM.render(
+// 	<App2 />,
+// 	document.getElementById("root6")
+// );
 
 ////////////////////////////////////////////////////////////
 
+import React from "react";
+import ReactDOM from "react-dom";
 import App3 from "./components/App.js";
+import data from "./testData.json";
+console.log(data);
 
 ReactDOM.render(
 	<App3 />,
 	document.getElementById("root7")
 );
+
+setTimeout(() => {
+	ReactDOM.render(
+		<h2>Forced React to unmount the App3 Component after 4s and re-render with this message!</h2>,
+		document.getElementById("root7")
+	);
+}, 4000);
--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,39 @@
 import React from "react";
 import Header3 from "./Header.js";
 
-const App3 = () => {
-	return(
-		<div className="App3">
-			<Header3 message3="Hello from a Component in a Separate Module / File" />
-			<div>
-				...
+// constructor(props) {
+// 	super(props);
+// 	this.state = {test: 42};
+// }
+// =
+// state = {test: 42};
+
+// the Component was successfully mounted to the DOM
+// componentDidMount() {
+
+// the Component is *about* to be unmounted
+// componentWillUnmount() {
+
+class App3 extends React.Component {
+	state = {
+		pageHeader: "Naming Contests"
+	};
+	componentDidMount() {
+		console.log("componentDidMount!");
+	}
+	componentWillUnmount() {
+		console.log("componentWillUnmount!");
+	}
+	render() {
+		return(
+			<div className="App3">
+				<Header3 message3={this.state.pageHeader} />
+				<div>
+					{this.state.test}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default App3;
